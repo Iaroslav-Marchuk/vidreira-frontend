f43b10f071cf9f7f0368095194631824a1d51748
iaroslav_marchuk <jarik.13a@gmail.com>
2025-11-23 19:31:15 +0000

    add work

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -150,7 +150,7 @@ i18n
     lng: savedLang,
     fallbackLng: 'pt',
     backend: {
-      loadPath: '/i18n/locales/{{lng}}/{{ns}}.json',
+      loadPath: '/src/i18n/locales/{{lng}}/{{ns}}.json',
     },
 
     ns: [
